78a445caee5faa6604952012a33ec09e5984b5f3
ramoh <mohantyrajesh1984@gmail.com>
2021-06-30 16:09:27 +0530

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    "extends": "plugin:vue/base",
+
+    "rules": {
+        "eqeqeq": "error",
+        "no-var": "error",
+        "prefer-const": "error",
+        "no-console": "off",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double",
+            "avoid-escape"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
